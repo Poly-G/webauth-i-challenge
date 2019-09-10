fb9168eefd32e3c00bba1e1d4937b033939c7214
Poly Goveia <poly.goveia@gmail.com>
2019-09-10 13:56:31 -0700

    declare sessionConfig

--- server.js
+++ server.js
@@ -13,4 +13,16 @@ server.get("/", (req, res) => {
   res.send("working");
 });
 
+const sessionConfig = {
+  name: "Shieda", //defaults to sid, provide name for security
+  secret: "Rhaast", // ?
+  cookie: {
+    maxAge: 1000 * 60, //set how long cookie is valid for in ms
+    secure: false, //true in production
+    httpOnly: true //always set this to true, denies js access to cookie
+  },
+  resave: false, //do we want to recreate a session if the session hasnt changed
+  saveUninitialized: false //ALWAYS FALSE BY DEFAULT: GDPR Laws against setting cookies auotmatically, require user consent
+};
+
 module.exports = server;
